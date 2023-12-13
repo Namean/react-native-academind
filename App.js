0e9c74aceb59a6d4fedb46c92627d69f5c8ff19a
namean <dwilsoncnc@gmail.com>
2023-12-13 15:25:16 -0500

    Styling React Native apps
    Applied styles, to React Native components
    using StyleSheet which provides validation, also React Native does not use CSS, but a JS like subset of CSS.

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text style={styles.dummyText}>
+        Hello World!
+      </Text>
+      <Button title="Tap me!" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'red',
+    
+  }
 });
--- DOCUMENTATION/notes.md
+++ DOCUMENTATION/notes.md
@@ -27,3 +27,38 @@ we have that file beacuse we're using expo
 ### App.js
 
 Its the only real code file we have in the project 
+
+
+
+# Styling React Native Apps
+
+
+There is no CSS!
+
+Inline Styles || StyleSheet Objects
+
+in the JavaScript coee files
+
+based on CSS and only a subset of properties
+
+style prop is only support on some elements
+
+
+```jsx
+
+<Text style={{
+    mar
+}}>Hello World!</Text>
+
+```
+
+
+React Native border
+
+valid keys:
+
+borderWidth: int
+borderColor: string || hex code, rgb, hsl, rgba
+
+besides auto-completion, the StyleSheet object also provides validation.
+If you would use invalid style properties or values, you would get an error / warning
